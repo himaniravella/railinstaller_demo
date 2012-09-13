9ade01abb3d5d7adc70d263c9c95572111671355
Himani Ravella <himani.ravella@cityofchicago.org>
2012-09-13 18:30:52 -0500

    Intitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '1e5378f505dada7505ee22d15c5c65eca51075748cb8ca3952502bd5211a10060b0e42c75a68a5529ae3fa0f6ace716692b480db119407f89b6116e6c1cf7875'
